c2372e5d1b665d45cdd09455af511c29b0006a5f
lupitaor <amartinez@itesa.edu.mx>
2026-09-08 14:49:41 -0600

    Versión_02: Recursividad factorial.

---
+++ Recursividad.Logica/Recursividades.cs
@@ -0,0 +1,30 @@
+﻿namespace Recursividad.Logica
+{
+    public class Recursividades
+    {
+
+        public int CalcularFactorial(int num)
+        {
+            //Caso base condición de salida
+
+            if (num == 1)
+            {
+                return 1;
+            }
+
+            //Caso general
+            return num = num * factorial(num - 1);
+        }
+
+        public int SumarNumeros(int numero)
+        {
+            //Caso Base o salida
+
+            //Caso general
+
+
+        }
+
+
+    }
+}
---
+++ Recursividad.UI.Consola/Program.cs
@@ -0,0 +1,15 @@
+﻿using Recursividad.Logica;
+
+Console.WriteLine("Ejemplo de recursividad");
+Console.WriteLine("\n\nCalcular el factorial");
+
+Recursividades oRecursividades = new Recursividades();
+
+int resultadoFactorial = oRecursividades.CalcularFactorial(5);
+Console.WriteLine("El resultado 5! es: " + resultadoFactorial);
+
+Console.WriteLine("\n\nSumar numeros");
+
+
+
+
